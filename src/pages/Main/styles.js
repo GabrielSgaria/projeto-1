a7435aaaf40871ff3265f30868aad0f1f271b01f
GabrielSgaria <gabrielsgaria2@gmail.com>
2024-03-07 09:24:57 -0300

    trash

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
+import React from "react";
 
 export const Container = styled.div`
   max-width: 700px;
@@ -34,9 +35,21 @@ export const Form = styled.form`
   }
 `;
 
-export const SubmitButton = styled.button.attrs({
+//Criando animação do botão
+const animate = keyframes`
+  from{
+    transform: rotate(0deg);
+  }
+
+  to{
+    transform: rotate(360deg)  
+  }
+`;
+
+export const SubmitButton = styled.button.attrs((props) => ({
   type: "submit",
-})`
+  disabled: props.loading,
+}))`
   background: #0d2636;
   border: 0;
   border-radius: 4px;
@@ -45,4 +58,56 @@ export const SubmitButton = styled.button.attrs({
   display: flex;
   justify-content: center;
   align-items: center;
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+
+  ${(props) =>
+    props.loading &&
+    css`
+      svg {
+        animation: ${animate} 2s linear infinite;
+      }
+    `}
+`;
+
+
+export const List = styled.ul`
+  list-style:none;
+  margin-top: 20px;
+
+  li{
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a{
+      color: #0D2636;
+      text-decoration: none;
+    }
+
+  }
+
+`;
+
+export const DeleteButton = styled.button.attrs({
+  type: 'button'
+})`
+
+  background: transparent;
+  color: #0d2636;
+  border:0;
+  padding: 8px 7px;
+  outline:0;
+  border-radius: 4px;
+
+
 `;
